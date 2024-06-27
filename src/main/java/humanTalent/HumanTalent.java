336c57d5b3fab8ca68b0347185ca3aea51100819
DavidUyaguariJ <david.uyaguari@udla.edu.ec>
2024-06-26 20:57:55 -0500

    fix: fix the images path

--- src/main/java/humanTalent/HumanTalent.java
+++ src/main/java/humanTalent/HumanTalent.java
@@ -1,13 +1,27 @@
 
 package humanTalent;
 
-import com.humanTalent.gui.Menu;
+import com.humanTalent.gui.Login;
+import com.humanTalent.logic.Employee;
+import com.humanTalent.logic.WorkArea;
+import com.humanTalent.persistance.Data;
 
 public class HumanTalent {
     public static void main(String [] args){
-        Menu menu = new Menu();
-        menu.setVisible(true);
-        menu.setLocationRelativeTo(null);
-        
+         //private String name;
+    //private String lastName;
+    //private String dni;
+    //private String phoneNumber;
+    //private WorkArea workArea;
+    //private boolean status;
+    //private double salary;
+
+        Data.workAreas.add(new WorkArea("RRHH"));
+        Data.workAreas.add(new WorkArea("OPERATIVO"));
+        WorkArea wa= Data.findWorkAreaByName(Data.workAreas, "RRHH");
+        Data.employees.add(new Employee("David","Uyaguari", "1856632547","0995632524",wa, true, 600.4 ));
+        Login log = new Login();
+        log.setVisible(true);
+        log.setLocationRelativeTo(null);
     }
 }
